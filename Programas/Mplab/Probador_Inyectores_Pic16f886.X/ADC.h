0ad30f3f022a37ce51028667522877d29106887c
Agustin586 <marcelo123z.com@gmail.com>
2020-12-16 03:08:08 -0300

    Se agrego el proyecto probador de inyectores

---
+++ Programas/Mplab/Probador_Inyectores_Pic16f886.X/ADC.h
@@ -0,0 +1,5 @@
+#include <xc.h>
+#define _XTAL_FREQ 20000000
+
+void Adc_init(void);
+int Adc(unsigned char canal);
---
+++ Programas/Mplab/Probador_Inyectores_Pic16f886.X/build/default/production/Main.d
@@ -0,0 +1,2 @@
+build/default/production\Main.p1: Main.c LCD_control.h ADC.h \
+  Task_Control.h Pwm_Soft.h
